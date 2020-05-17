241ac8ff7a6de972c5a326fa9921fb76cb8c4605
kcemenike <29731161+kcemenike@users.noreply.github.com>
2020-05-17 21:04:49 +0100

    lesson 3

---
+++ _07_modules.py
@@ -0,0 +1,23 @@
+# MODULES
+# In Python, you can import modules to do specialised functions
+# The math module, for example, is used for mathematical computation
+# You can also import your own .py files as modules
+
+# To get the square root of a number we need to import math
+# let us also import our own file as a module
+import math
+import _06_conditionals
+
+number = 625
+square_root = math.sqrt(number)
+print(square_root)
+
+# You notice that importing a module runs the module and all its
+# contents to make the variables in the module available
+# importing _06_conditionals automatically makes the dictionary available
+
+# We can now get the variables in the _06_conditionals module
+# e.g. the threshold_age
+print(_06_conditionals.threshold_age)
+
+print(dir(_06_conditionals))  # Allows us to get all objects in the module
---
+++ _08_input.py
@@ -0,0 +1,21 @@
+# INPUT
+
+input("Please enter a number:\n")
+# Gets input while presenting a prompt to the user
+# Can be assigned to a variable
+# \n is an escape character used to print a new line
+# Common escape characters are
+# \n line feed
+# \ ignore newline: used when your code spans multiple lines
+# \t tab
+# \\ backslash
+# \' single quote
+
+age = input("Please enter your age:\n")
+print(type(age))
+# input always returns a string
+# You'll need to convert to any type you need
+
+age = int(input("Please enter your age:\n"))
+print(age)
+print(type(age))
